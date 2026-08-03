26ee39bbb9868266c48dbeda147d1c66ef19f488
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-03 15:55:13 +0600

    Add 3597020.lua from 3597020.zip

---
+++ 3597020.lua
@@ -0,0 +1,25 @@
+-- 3597020's Lua and Manifest Created by Morrenus
+-- Becoming a King
+-- Created: November 15, 2025 at 09:12:15 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(3597020) -- Becoming a King
+-- MAIN APP DEPOTS
+addappid(3597021, 1, "c19caf5a7bf6c6240363c9aebdf31b185b4cc2662397ce352678f0bdaaadc35a") -- Depot 3597021
+setManifestid(3597021, "9043454255360608214", 6231400776)
+-- DLCS WITH DEDICATED DEPOTS
+-- Becoming a King - Digital Artbook (AppID: 4072680)
+addappid(4072680)
+addappid(4072681, 1, "b33d16c5a46f12a5fc1af82f4fd123e3f75c596bcc9efe23e6376faee44f7bda") -- Becoming a King - Digital Artbook - Depot 4072681
+setManifestid(4072681, "496602665802719239", 321168079)
+-- Becoming a King - Animations Pack (AppID: 4072690)
+addappid(4072690)
+addappid(4072691, 1, "960f2399eb302a6e01aab03a900a1cc77a631b95101e9dad3164313cff1f4ff0") -- Becoming a King - Animations Pack - Depot 4072691
+setManifestid(4072691, "7939445868875113236", 10356604176)
+-- Becoming a King - Wallpapers Pack (AppID: 4072700)
+addappid(4072700)
+addappid(4072701, 1, "da0aeb851d78091cf5a2542a7bc098fb34915d32d991d38e86359a614bc4a5f0") -- Becoming a King - Wallpapers Pack - Depot 4072701
+setManifestid(4072701, "4745372408192038845", 238596512)
