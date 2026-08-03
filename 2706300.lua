57f5bf1a3e20d5ffda0e3f83325f763ee91fb3c1
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-03 15:55:05 +0600

    Add 2706300.lua from 2706300.zip

---
+++ 2706300.lua
@@ -0,0 +1,39 @@
+-- 2706300's Lua and Manifest Created by Hubcap Manifest
+-- MILF's Plaza
+-- Created: February 10, 2026 at 22:24:56 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 9
+-- Total DLCs: 5
+
+-- MAIN APPLICATION
+addappid(2706300, 1, "dd6719639c0d8e07684062aa65eaa427b41e24027afe69ef16f211daf3bf5511") -- MILF's Plaza
+-- MAIN APP DEPOTS
+addappid(2706301, 1, "410cae000a800de807a1b1972880f3c6810577b02984996ce4624afba5707233") -- Depot 2706301
+setManifestid(2706301, "5585069718476797398", 2104927389)
+addappid(2706302, 1, "8749578373b02a7f5195daec25f5b847e7c375188fb42522e134134bac12cff3") -- Depot 2706302
+setManifestid(2706302, "4291584476051264757", 2093163663)
+-- DLCS WITH DEDICATED DEPOTS
+-- MILFs Plaza - Juicy Wallpapers Pack (AppID: 2814100)
+addappid(2814100)
+addappid(2814100, 1, "75285352d2e90ee65a97e3123a7abe02d2739ee2382027567c386fccceb9ae31") -- MILFs Plaza - Juicy Wallpapers Pack - Depot 2814100
+setManifestid(2814100, "1078243213435934783", 209697670)
+-- MILFs Plaza - Digital Artbook (AppID: 2814110)
+addappid(2814110)
+addappid(2814110, 1, "1023441950b95d0576b096514e05ff00fbd3ffcd3ba3efa121fb67a253b3166c") -- MILFs Plaza - Digital Artbook - Depot 2814110
+setManifestid(2814110, "8794330003497708556", 636350716)
+-- MILFs Plaza - Hot Animations Pack (AppID: 2814120)
+addappid(2814120)
+addappid(2814120, 1, "206914d88d77e711305d1e55c31fa84597e9f5976ed7403df58bafb73fa46311") -- MILFs Plaza - Hot Animations Pack - Depot 2814120
+setManifestid(2814120, "7096570944124010598", 462633321)
+-- MILFs Plaza - Bonus Content (AppID: 2975620)
+addappid(2975620)
+addappid(2975620, 1, "25eaae474555219c50666945f1ab5be6859a71892f78fa62770634e28b3bc8c2") -- MILFs Plaza - Bonus Content - Depot 2975620
+setManifestid(2975620, "4153078243675908724", 127885611)
+addappid(2975621, 1, "4c301a1d3d7876c162150363fc7f994b4ca364ec0a598f3554bf42ea63b89130") -- MILFs Plaza - Bonus Content - Depot 2975621
+setManifestid(2975621, "4518657139967449197", 2073995559)
+-- MILFs Plaza - Beach Episode (AppID: 3283090)
+addappid(3283090)
+addappid(3283090, 1, "ff57df5770f283685d2d085d99d75a442a8b8ce5ddd8ca2830cfa6122c6e7e12") -- MILFs Plaza - Beach Episode - Depot 3283090
+setManifestid(3283090, "1477609026355607465", 411861692)
+addappid(3283091, 1, "0f8dd67ffa0e76b5ce43a3c76e7c9cf061b414302e05b9a82bc81c498d01e148") -- MILFs Plaza - Beach Episode - Depot 3283091
+setManifestid(3283091, "5108947559828910283", 411873988)
