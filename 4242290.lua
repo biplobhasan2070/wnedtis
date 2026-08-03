8f6f0cd8b8c9b067df73e5d7decca3f2b4c40ed5
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-03 17:25:07 +0600

    Add 4242290.lua for AppID 4242290

---
+++ 4242290.lua
@@ -0,0 +1,12 @@
+-- 4242290's Lua and Manifest Created by Hubcap Manifest
+-- SPITE: Taking the Boss's Wife, Sister and Mother
+-- Created: May 25, 2026 at 02:16:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4242290) -- SPITE: Taking the Boss's Wife, Sister and Mother
+-- MAIN APP DEPOTS
+addappid(4242291, 1, "193f712b8c3e06b1d21b9073523678087406578590945666d73079a628e08f6c") -- Depot 4242291
+setManifestid(4242291, "3086716412332806390", 831139639)
