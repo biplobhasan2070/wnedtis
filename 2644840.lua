1c37289ff26c809740ecdb09e599c36701dc71df
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-05 11:16:35 +0600

    Add 2644840.lua for AppID 2644840

---
+++ 2644840.lua
@@ -0,0 +1,14 @@
+-- 2644840's Lua and Manifest Created by Hubcap Manifest
+-- 廃村散策会活動記録
+-- Created: August 04, 2026 at 22:44:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2644840) -- 廃村散策会活動記録
+-- MAIN APP DEPOTS
+addappid(2644841, 1, "9d5ea4f241aed81462ebb0a628ffc78ed20efd28fcf98ff65e3f5c8564f21b6d") -- Depot 2644841
+setManifestid(2644841, "5958826031338446796", 736375200)
+addappid(2644843, 1, "33b1d3733bb24d2b3c3911a1a6f2e453b6bc2dfb955c13993c222534b29b479c") -- Depot 2644843
+setManifestid(2644843, "6113800188406028416", 651660887)
