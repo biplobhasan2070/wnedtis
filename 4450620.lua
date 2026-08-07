43c6f8670d692a65fe5d069e3ae5ea1e14ab5222
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-07 23:13:26 +0600

    Add 4450620.lua for AppID 4450620

---
+++ 4450620.lua
@@ -0,0 +1,18 @@
+-- 4450620's Lua and Manifest Created by Hubcap Manifest
+-- GRAIN ROT
+-- Created: August 07, 2026 at 11:23:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4450620, 1, "635838748ea51cd342b679f81c24c0da0c9b2f4d038ba45cbc3068cf03dfaf93") -- GRAIN ROT
+-- MAIN APP DEPOTS
+addappid(4450621, 1, "76eb98135a9676ffb42f3b3663a3785045bdb22af29f92034e509d5423217b04") -- Depot 4450621
+setManifestid(4450621, "5982775281597055314", 4500489988)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
