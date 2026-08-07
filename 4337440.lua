3584a025ee5addf6452de30a99e1f41e84495654
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-07 09:04:59 +0600

    Add 4337440.lua for AppID 4337440

---
+++ 4337440.lua
@@ -0,0 +1,12 @@
+-- 4337440's Lua and Manifest Created by Hubcap Manifest
+-- Memory of Memorie : A Chill Story
+-- Created: August 06, 2026 at 13:37:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4337440) -- Memory of Memorie : A Chill Story
+-- MAIN APP DEPOTS
+addappid(4337441, 1, "3f8fb014aeabd0f928b82f8d2f4d866ea02c977a5d863e8a2471a7d233ca584b") -- Depot 4337441
+setManifestid(4337441, "2062692072016840022", 625934894)
