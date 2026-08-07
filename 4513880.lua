c855bfe4aba891afac6411ef4511311a82a41340
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-07 23:13:22 +0600

    Add 4513880.lua for AppID 4513880

---
+++ 4513880.lua
@@ -0,0 +1,12 @@
+-- 4513880's Lua and Manifest Created by Hubcap Manifest
+-- 难道你是GAL高手
+-- Created: August 07, 2026 at 11:09:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4513880) -- 难道你是GAL高手
+-- MAIN APP DEPOTS
+addappid(4513881, 1, "6461ee2c7a6679cce446b4ef8dd85820ef19afec5fc0bbb1c42b5e2404a6a5e7") -- Depot 4513881
+setManifestid(4513881, "4342070033433212396", 10510713275)
