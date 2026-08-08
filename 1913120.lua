0484a111d37840a031ef53cd83d3e186fbaa0d01
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-08 21:47:59 +0600

    Add 1913120.lua for AppID 1913120

---
+++ 1913120.lua
@@ -0,0 +1,5 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(1913120)
+addappid(1913121,0,"ef09faa539a0032c86e138039553cfb95ed24ac0a3af18e948854bf7d108cbea")
+setManifestid(1913121,"3797557467315138068")
+-- Discord: https://discord.gg/Xx2qdXCvKY
