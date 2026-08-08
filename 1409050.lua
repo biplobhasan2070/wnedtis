7a873e5a5bdf6cd9bd5700aabfe356172d3ed979
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-08 10:44:36 +0600

    Add 1409050.lua for AppID 1409050

---
+++ 1409050.lua
@@ -0,0 +1,6 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(1409050)
+addtoken(1409050, "10034039316830785954")
+addappid(1409051, 1, "a7a4d10a114c56490fce69fa305bbcb594a87a608a5144b74d2375a7df9c3f24")
+setManifestid(1409051, "4789767955381178332", 404542763)
+-- Discord: https://discord.gg/Xx2qdXCvKY
