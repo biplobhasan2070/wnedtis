5cf7dfbe992fcaf6e8d96cca24d5003ad2736216
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-10 02:23:54 +0600

    Add 4828740.lua for AppID 4828740

---
+++ 4828740.lua
@@ -0,0 +1,12 @@
+-- 4828740's Lua and Manifest Created by Hubcap Manifest
+-- Cover Story
+-- Created: August 09, 2026 at 09:46:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4828740) -- Cover Story
+-- MAIN APP DEPOTS
+addappid(4828741, 1, "c4fd2eb8ee88414f7c298b8159ed3fdaa1fa927abc92dc0ca2f2d8e5c41d2a8b") -- Depot 4828741
+setManifestid(4828741, "3102887067526805290", 1082122309)
