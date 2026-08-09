184b353efb3445c8c058797b191fe5594e935298
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-10 02:23:54 +0600

    Add 4521430.lua for AppID 4521430

---
+++ 4521430.lua
@@ -0,0 +1,12 @@
+-- 4521430's Lua and Manifest Created by Hubcap Manifest
+-- Oblivion Maiden
+-- Created: August 09, 2026 at 09:47:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4521430) -- Oblivion Maiden
+-- MAIN APP DEPOTS
+addappid(4521431, 1, "c70cfd937a8865e728368760b429100772836c31fb5091117774f27a7db29a5e") -- Depot 4521431
+setManifestid(4521431, "3832273892574040771", 875649104)
