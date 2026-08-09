17a41c18dcb77540b605efd12133d908e6335e31
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-10 05:32:36 +0600

    Add 3576350.lua for AppID 3576350

---
+++ 3576350.lua
@@ -0,0 +1,7 @@
+addappid(3576350) -- Sex, Secrets & Used Tech
+addappid(3576351, 1, "a81a90790f79399b98a8563d1cb099b157662333dd4bac8c1a0060086797e56b") -- Depot 3576351
+setManifestid(3576351, "1110424412952841038", 1752550658)
+-- Sex, Secrets  Used Tech - Digital Artbook (AppID: 4687240)
+addappid(4687240)
+addappid(4687240, 1, "744448e72018b51db6274ce8a99abdce9989f5d69f64c6da501a77e970363d7f") -- Sex, Secrets  Used Tech - Digital Artbook - Depot 4687240
+setManifestid(4687240, "2351578736436094779", 33748690)
