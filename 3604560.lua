ea39e2a60e75c3d437a3c435c084d19172cdaef4
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-11 16:26:58 +0600

    Add 3604560.lua for AppID 3604560

---
+++ 3604560.lua
@@ -0,0 +1,5 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(3604560)
+addappid(3604561, 1, "d03520f4a6486c7f802870eb7e44c7268dd1d657627cd9ec2cd3488f7bdbfa66")
+setManifestid(3604561, "2860273244056030584", 2446478348)
+-- Discord: https://discord.gg/Xx2qdXCvKY
