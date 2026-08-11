63e821e339a0c5692347701ec93c929e09edec94
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-11 16:26:58 +0600

    Add 1067360.lua for AppID 1067360

---
+++ 1067360.lua
@@ -0,0 +1,6 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(1067360)
+addtoken(1067360, "18077378792152236908")
+addappid(1067361, 1, "23f1500469c517a7c93decc37b90c10f898209b03d691861820093164f4a1db7")
+setManifestid(1067361, "3020511451103679008", 33790444870)
+-- Discord: https://discord.gg/Xx2qdXCvKY
