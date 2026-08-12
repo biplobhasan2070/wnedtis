dd2055d178c5ea24b84871fcc94a2f90cd0c85c6
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-12 11:32:03 +0600

    Add 3762720.lua for AppID 3762720

---
+++ 3762720.lua
@@ -0,0 +1,12 @@
+-- 3762720's Lua and Manifest Created by Hubcap Manifest
+-- MY WIFE IS A FOOTBALL COACH
+-- Created: August 12, 2026 at 00:04:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3762720, 1, "6c4f4f295a93d4fcf9a1984496f1e4e4de3b3c3bd9e86f5f5ca926117aa1b3e9") -- MY WIFE IS A FOOTBALL COACH
+-- MAIN APP DEPOTS
+addappid(3762721, 1, "f77e4fc4230fed328801378a41819c93432deb1e30e27a4a2c9bd16ea2de9618") -- Depot 3762721
+setManifestid(3762721, "5970291522585294804", 439738301)
