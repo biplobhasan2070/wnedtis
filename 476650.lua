221b3d30448d56ca562da02f6cec5750b3f1e666
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-13 09:51:08 +0600

    Add 476650.lua for AppID 476650

---
+++ 476650.lua
@@ -0,0 +1,21 @@
+-- 476650's Lua and Manifest Created by Hubcap Manifest
+-- The Silver Case
+-- Created: October 01, 2025 at 00:50:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(476650) -- The Silver Case
+-- MAIN APP DEPOTS
+addappid(476651, 1, "3523e73546fd4c2a6a91804e5067f10277297887e37693c5b63b5de2b064796d") -- The Silver Case Content
+setManifestid(476651, "8300769584640698677", 6750692297)
+addappid(476652, 1, "4cfa9248ca344c1f4a2b787b32fbcffba68523e0ca8247770f68226e8024d3d8") -- The Silver Case for Mac
+setManifestid(476652, "1252734989029789737", 6777882812)
+addappid(476653, 1, "47c3ddcc66ddd1fa80d4afed0bb0884cae5865c9e11fffa2fc9e730e1ecfc304") -- The Silver Case for Linux
+setManifestid(476653, "435095679675438413", 6793815477)
+-- DLCS WITH DEDICATED DEPOTS
+-- The Silver Case The Extra Contents (AppID: 533570)
+addappid(533570)
+addappid(533570, 1, "212e3ebeef7b649d3968f18b1e67ac1ec45a7201a9b69a28cf78aa6f56ba28c6") -- The Silver Case The Extra Contents - The Silver Case: The Deluxe Pack (533570) Depot
+setManifestid(533570, "3719891408426837923", 1516880020)
