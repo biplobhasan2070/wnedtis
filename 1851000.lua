112fabc2010606f04df5a9f9bf18b5368afc36c7
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-14 15:34:54 +0600

    Add 1851000.lua for AppID 1851000

---
+++ 1851000.lua
@@ -0,0 +1,7 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(1851000, 1, "5b6b4da88b8d154f93fcca2f40309f2cee34362652a711af7834a6d0fcb89944")
+addappid(1851001, 1, "ffe5b1ece7e5b96c85c653b86d9911c4902cba6bab47dfef5cb42a5c85115ec4")
+setManifestid(1851001, "1155572757969386099", 869874787)
+addappid(1851002, 1, "b9328bc4727e36dd55b1d70891b88a2084d8c94c8a20a25a064a7c2dbd81abd5")
+setManifestid(1851002, "5084041017333384969", 771616570)
+-- Discord: https://discord.gg/Xx2qdXCvKY
