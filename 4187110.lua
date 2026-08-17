08c48e0c3853230a7f0bd5f89e3576bdd916cf91
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-17 10:05:31 +0600

    Add 4187110.lua for AppID 4187110

---
+++ 4187110.lua
@@ -0,0 +1,14 @@
+-- 4187110's Lua and Manifest Created by Hubcap Manifest
+-- Autocard - Your RPG Adventure
+-- Created: August 16, 2026 at 07:41:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4187110, 1, "fb8a090e4b6c47d619dd53f13bdc3cef796fbc86aabd3e582626fbd4c2891ca3") -- Autocard - Your RPG Adventure
+-- MAIN APP DEPOTS
+addappid(4187111, 1, "4a6f862813e68a890208886c143e4e900f09af81bfe2aa8f7a972b0d4ecf2c8b") -- Depot 4187111
+setManifestid(4187111, "6402174197083292792", 354000376)
+addappid(4187112, 1, "13a7f949665c25608028173f299923c87c3b6058d8b55f770069d4fb833c444e") -- Depot 4187112
+setManifestid(4187112, "1619690699516988682", 319658680)
