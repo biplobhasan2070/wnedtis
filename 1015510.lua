20454238241abc26b8026a57e5056384ae63ded8
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-17 10:05:32 +0600

    Add 1015510.lua for AppID 1015510

---
+++ 1015510.lua
@@ -0,0 +1,19 @@
+-- 1015510's Lua and Manifest Created by Hubcap Manifest
+-- Dr. Tanya`s Experiment
+-- Created: August 16, 2026 at 12:07:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1015510) -- Dr. Tanya`s Experiment
+addtoken(1015510, "1922378169543717242")
+-- MAIN APP DEPOTS
+addappid(1015511, 1, "710d23282c7a240c37d3504bd5b0918f464e376ed355f5cb07abf04fadcf10d3") -- Depot 1015511
+setManifestid(1015511, "3821632877111079970", 3400594221)
+-- SHARED DEPOTS (from other apps)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- VC 2017 Redist (Shared from App 228980)
+setManifestid(228987, "4302102680580581867", 29664201)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
