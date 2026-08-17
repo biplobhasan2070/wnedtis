c42bcc69a37422e0b6caa42cd36ea1c5ccfba02b
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-17 10:05:31 +0600

    Add 3946360.lua for AppID 3946360

---
+++ 3946360.lua
@@ -0,0 +1,12 @@
+-- 3946360's Lua and Manifest Created by Hubcap Manifest
+-- Blightseed
+-- Created: August 16, 2026 at 12:59:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3946360) -- Blightseed
+-- MAIN APP DEPOTS
+addappid(3946361, 1, "344bc23a02227be4bb8f7ed7b829f46201e8ce4c7e6c6a87fee7b931c3609b5a") -- Depot 3946361
+setManifestid(3946361, "5634694536596050171", 342783968)
