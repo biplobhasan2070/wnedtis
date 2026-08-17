1d2e6e4ee03f93a6c1b6a00dd04e6d8a23ee50ab
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-17 10:05:31 +0600

    Add 1032100.lua for AppID 1032100

---
+++ 1032100.lua
@@ -0,0 +1,19 @@
+-- 1032100's Lua and Manifest Created by Hubcap Manifest
+-- Boarding College 2
+-- Created: August 16, 2026 at 12:07:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1032100) -- Boarding College 2
+addtoken(1032100, "10183329689991416516")
+-- MAIN APP DEPOTS
+addappid(1032101, 1, "2709b6b99b2b8e6b1fa6be5dd039b87a3d3b1622aef97cfbb480b88331d02c8e") -- Depot 1032101
+setManifestid(1032101, "6304383594724213824", 2049541803)
+-- SHARED DEPOTS (from other apps)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- VC 2017 Redist (Shared from App 228980)
+setManifestid(228987, "4302102680580581867", 29664201)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
