0ed2b807a594a0a004b36ec19528e8ffae076e4f
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-19 01:49:10 +0600

    Add 4317790.lua for AppID 4317790

---
+++ 4317790.lua
@@ -0,0 +1,12 @@
+-- 4317790's Lua and Manifest Created by Hubcap Manifest
+-- Last Pirates: Die Together
+-- Created: August 18, 2026 at 13:17:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4317790) -- Last Pirates: Die Together
+-- MAIN APP DEPOTS
+addappid(4317791, 1, "3d1c1770af5234972a92eb34df6e9b32b6d44f45ae02e1fdce4760f843c2206d") -- Depot 4317791
+setManifestid(4317791, "2647469536243314190", 4216365916)
