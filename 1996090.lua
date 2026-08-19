e5e8fac9fc3d17ad7704a8944dd6cbf80d53007a
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-19 18:21:51 +0600

    Add 1996090.lua for AppID 1996090

---
+++ 1996090.lua
@@ -0,0 +1,12 @@
+-- 1996090's Lua and Manifest Created by Hubcap Manifest
+-- Vapor World: Over The Mind
+-- Created: August 19, 2026 at 00:55:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1996090) -- Vapor World: Over The Mind
+-- MAIN APP DEPOTS
+addappid(1996091, 1, "ddb21729a2e8eebf759ffc8978e92151736934a682fddc239f3ef46b9f64729a") -- Depot 1996091
+setManifestid(1996091, "8836405688764788670", 9171361353)
