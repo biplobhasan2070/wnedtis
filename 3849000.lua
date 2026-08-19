fb24054ec3c9bdd9dfd4c1ab9b26954458dea3bf
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-19 21:08:06 +0600

    Add 3849000.lua for AppID 3849000

---
+++ 3849000.lua
@@ -0,0 +1,18 @@
+-- 3849000's Lua and Manifest Created by Hubcap Manifest
+-- The Lantern of the Laughless Saint
+-- Created: August 19, 2026 at 06:57:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3849000, 1, "6148a67e06f29c387f845b8e90f2e84cc5ec0aaf7c5ad44d7a89995bb89cb437") -- The Lantern of the Laughless Saint
+-- MAIN APP DEPOTS
+addappid(3849001, 1, "aa90d31e86524ce3148c53b9676743fa810f435b25e1037c62ec60e88c861ccd") -- Depot 3849001
+setManifestid(3849001, "3523186864376888586", 8486691325)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
