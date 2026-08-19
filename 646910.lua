c9211f2970ef9e06f620e257f5bca96ec5af1c11
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-19 21:08:02 +0600

    Add 646910.lua for AppID 646910

---
+++ 646910.lua
@@ -0,0 +1,60 @@
+-- 646910's Lua and Manifest Created by Hubcap Manifest
+-- The Crew 2
+-- Created: August 19, 2026 at 06:23:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 38
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(646910, 1, "1944093dfad0d4adf98cf34dbb4b0206111283004b6d26f280de63deb7844ce4") -- The Crew 2
+-- MAIN APP DEPOTS
+addappid(646911, 1, "cc9f8bf715fb14e78193c2cdc408aabe45916e645246197afcc4f361b19eeb99") -- The Crew 2
+setManifestid(646911, "2026593396695106824", 38255717881)
+-- SHARED DEPOTS (from other apps)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") -- Ubisoft Connect PC Client Content (Shared from App 1716750)
+setManifestid(1716751, "8936186649444731600", 264933784)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(702230) -- The Crew 2 - Standard Preorder - Uplay Activation
+addtoken(702230, "13441490310662342226")
+addappid(702240) -- The Crew 2 - Deluxe Preorder - Uplay Activation
+addtoken(702240, "4940938489702710108")
+addappid(702250) -- The Crew 2 - Gold Preorder - Uplay Activation
+addappid(889870) -- The Crew 2 - Standard Edition - Uplay Activation
+addappid(889871) -- The Crew 2 - Deluxe Edition - Uplay Activation
+addappid(889872) -- The Crew 2 - Gold Edition - Uplay Activation
+addappid(889880) -- The Crew 2 - Season Pass - Uplay Activation
+addtoken(889880, "11890041929745353930")
+addappid(889890) -- The Crew 2 - Season Pass
+addappid(999420) -- The Crew 2 - Full Game ownership
+addappid(999421) -- The Crew 2 - Free weekend ownership
+addappid(1688450) -- The Crew 2 - Special Edition - Ubisoft Activation
+addappid(1688451) -- The Crew 2 - New Gold Edition - Ubisoft Activation
+addappid(2183880) -- The Crew 2 - Porsche Cayman GT4 2016 Starter Pack
+addappid(2183881) -- TC2 - Porsche Cayman GT4 2016 Starter Pack - Ubi Activation
+addappid(2183882) -- The Crew 2 - Mazda RX8 Starter Pack
+addappid(2183883) -- TC2 - Mazda RX8 Starter Pack - Ubi Activation
+addappid(4311250) -- The Crew 2 - American Summit Pack
+addappid(4311260) -- The Crew 2 - American Summit Pack - Ubisoft Activation
+addappid(4446240) -- The Crew 2 - European Summit Pack
+addappid(4446250) -- The Crew 2 - European Summit Pack - Ubisoft Activation
+addappid(4446260) -- The Crew 2 - Japanese Summit Pack
+addappid(4446270) -- The Crew 2 - Japanese Summit Pack - Ubisoft Activation
+addappid(4868970) -- The Crew 2 - Nissan Car Pack
+addappid(4868980) -- The Crew 2 - Nissan Car Pack - Ubisoft Activation
+addappid(4868990) -- The Crew 2 - Porsche Car Pack
+addappid(4869000) -- The Crew 2 - Porsche Car Pack - Ubisoft Activation
+addappid(4869010) -- The Crew 2 - BMW Car Pack
+addappid(4869020) -- The Crew 2 - BMW Car Pack - Ubisoft Activation
+addappid(4869030) -- The Crew 2 - Audi Car Pack
+addappid(4869040) -- The Crew 2 - Audi Car Pack - Ubisoft Activation
+addappid(4869050) -- The Crew 2 - Mercedes Car Pack
+addappid(4869060) -- The Crew 2 - Mercedes Car Pack - Ubisoft Activation
+addappid(4869080) -- The Crew 2 - Bike Pack
+addappid(4869090) -- The Crew 2 - Bike Pack - Ubisoft Activation
+addappid(4869100) -- The Crew 2  Sky, Dirt, and Waves Pack
+addappid(4869110) -- The Crew 2  Sky, Dirt, and Waves Pack - Ubisoft Activation
+addappid(4869120) -- The Crew 2 - Mega Pack
+addappid(4869130) -- The Crew 2  Mega Car Pack - Ubisoft Activation
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(646912) -- Depot 646912 (empty depot)
