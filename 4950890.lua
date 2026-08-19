d00c73638b9493740f1102d9fdbc36a0d364922f
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-19 18:21:51 +0600

    Add 4950890.lua for AppID 4950890

---
+++ 4950890.lua
@@ -0,0 +1,12 @@
+-- 4950890's Lua and Manifest Created by Hubcap Manifest
+-- Tropical Hop
+-- Created: August 19, 2026 at 01:18:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4950890) -- Tropical Hop
+-- MAIN APP DEPOTS
+addappid(4950891, 1, "2be97ba1628719e58edd2b79285309ea8eb9aa61bfaf22d9a36c11a1ade81bb1") -- Depot 4950891
+setManifestid(4950891, "7555562596290770958", 85588141)
