1dc89cb90c38ece3264786ca622e01f2175d12ac
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-19 18:21:51 +0600

    Add 3435610.lua for AppID 3435610

---
+++ 3435610.lua
@@ -0,0 +1,12 @@
+-- 3435610's Lua and Manifest Created by Hubcap Manifest
+-- Exark
+-- Created: August 19, 2026 at 01:18:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3435610, 1, "04081d2d68ac6ae79fc758bf2c5af2f157aa7bae3a46232daca58d923463bc7f") -- Exark
+-- MAIN APP DEPOTS
+addappid(3435611, 1, "8d8fb238e9c2af0af6c2d74a8032b3a762eececaf708780272c76cb0e8b38c06") -- Depot 3435611
+setManifestid(3435611, "3157462133135320036", 1579848076)
