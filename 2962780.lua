8423461a88f29e429d07d6728a7a354a74c6dd06
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-19 18:21:55 +0600

    Add 2962780.lua for AppID 2962780

---
+++ 2962780.lua
@@ -0,0 +1,18 @@
+-- 2962780's Lua and Manifest Created by Hubcap Manifest
+-- BeastLink
+-- Created: August 19, 2026 at 01:17:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2962780, 1, "b1fb2905fda5af39bd95905a51c7c628249cdf95858b760aef78de62581c69fc") -- BeastLink
+-- MAIN APP DEPOTS
+addappid(2962781, 1, "e68d15ff207c09d4e8cb1845d3b3138307609415a66e4244b208a9a7ef138d6f") -- Depot 2962781
+setManifestid(2962781, "4976660361101299588", 32478348243)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
