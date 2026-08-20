ea8a130a6e3a98ba10efa2c9d82a50fdae8161e7
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-20 17:43:28 +0600

    Add 3726730.lua for AppID 3726730

---
+++ 3726730.lua
@@ -0,0 +1,14 @@
+-- 3726730's Lua and Manifest Created by Hubcap Manifest
+-- Dreamcards
+-- Created: August 20, 2026 at 05:25:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3726730) -- Dreamcards
+-- MAIN APP DEPOTS
+addappid(3726731, 1, "f80577dbebff25956bcb4dfd6d0fdf709995aa5e106f8814b30224bce1f5a2ec") -- Depot 3726731
+setManifestid(3726731, "7724615056389648741", 538023267)
+addappid(3726732, 1, "c1c496d5157d98332cdde325297a2c34bf5acdc82bb263eec41ee7851a36fcc5") -- Depot 3726732
+setManifestid(3726732, "9215299319542256256", 506569075)
