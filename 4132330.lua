9f8a500f11326b13d7f32a5db54435359105b8df
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-20 17:43:32 +0600

    Add 4132330.lua for AppID 4132330

---
+++ 4132330.lua
@@ -0,0 +1,18 @@
+-- 4132330's Lua and Manifest Created by Hubcap Manifest
+-- Pizza House Simulator🍕
+-- Created: August 20, 2026 at 06:17:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4132330, 1, "30a655c7b301177576fc1072673bb9c5744de0f281229d44c77c40a37c659259") -- Pizza House Simulator🍕
+-- MAIN APP DEPOTS
+addappid(4132331, 1, "51035a357a681b45f9b70402c409d25181faea5a48999aaf9eb13536d0a203e9") -- Depot 4132331
+setManifestid(4132331, "3468124998531884168", 4897564208)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
