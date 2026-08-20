2f32446ff963a69e8a47831739210ccebbfc913c
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-20 17:43:28 +0600

    Add 3553250.lua for AppID 3553250

---
+++ 3553250.lua
@@ -0,0 +1,12 @@
+-- 3553250's Lua and Manifest Created by Hubcap Manifest
+-- Holey
+-- Created: August 20, 2026 at 06:17:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3553250, 1, "3b6ab5e86b05669f0ac1e309dc3a34039e71fff08268aed3406b25095fc6080f") -- Holey
+-- MAIN APP DEPOTS
+addappid(3553251, 1, "623f46c0886d2d8472d9e716839c646661432f26614dd89691333084d8eb63c0") -- Depot 3553251
+setManifestid(3553251, "4199565704056890808", 655324871)
