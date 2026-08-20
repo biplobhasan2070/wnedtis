db4190cfb2bd9e5ed2c53e0e9ab328edc832f693
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-20 17:43:33 +0600

    Add 4532550.lua for AppID 4532550

---
+++ 4532550.lua
@@ -0,0 +1,18 @@
+-- 4532550's Lua and Manifest Created by Hubcap Manifest
+-- 守护我的大火箭
+-- Created: August 20, 2026 at 04:32:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4532550) -- 守护我的大火箭
+-- MAIN APP DEPOTS
+addappid(4532551, 1, "31b16714b622d020aa18d8fe4291bce7150e880f4f50060316f921c590535f39") -- Depot 4532551
+setManifestid(4532551, "8771896501717778521", 1898714612)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
