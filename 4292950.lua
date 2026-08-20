9dd9dde75e7c35d1f62feec2856b13d340a67863
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-20 17:43:27 +0600

    Add 4292950.lua for AppID 4292950

---
+++ 4292950.lua
@@ -0,0 +1,14 @@
+-- 4292950's Lua and Manifest Created by Hubcap Manifest
+-- Shao Song I
+-- Created: August 20, 2026 at 05:21:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4292950) -- Shao Song I
+-- MAIN APP DEPOTS
+addappid(4292951, 1, "d67ee3c314a005c060fc2616ff7d3787bd366d0b33bcc961bb42eab401a4bf8a") -- Depot 4292951
+setManifestid(4292951, "3408645548648670524", 41732150595)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4292952) -- Depot 4292952 (empty depot)
