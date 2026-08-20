78c6866bda70c8b50456686d455d1d62ca666ffb
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-20 17:43:28 +0600

    Add 4458840.lua for AppID 4458840

---
+++ 4458840.lua
@@ -0,0 +1,14 @@
+-- 4458840's Lua and Manifest Created by Hubcap Manifest
+-- T.W.I.R.L.
+-- Created: August 19, 2026 at 13:49:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4458840) -- T.W.I.R.L.
+-- MAIN APP DEPOTS
+addappid(4458841, 1, "549d855a38d0d749c983d548a497457864e1ed8ab7441f484cfda548303df772") -- Depot 4458841
+setManifestid(4458841, "4105915196140485330", 876823263)
+addappid(4458842, 1, "c343a98c65af60786d8f0177ee82e1e596bf98154569ea50ed3b6fad2b103055") -- Depot 4458842
+setManifestid(4458842, "3973569033202446893", 895279071)
