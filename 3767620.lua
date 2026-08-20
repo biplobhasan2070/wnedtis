9cef6062e0e3f3ae925ac2cf7d5303f251e21421
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-20 17:43:28 +0600

    Add 3767620.lua for AppID 3767620

---
+++ 3767620.lua
@@ -0,0 +1,12 @@
+-- 3767620's Lua and Manifest Created by Hubcap Manifest
+-- Harvest Yuuka
+-- Created: August 19, 2026 at 17:34:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3767620) -- Harvest Yuuka
+-- MAIN APP DEPOTS
+addappid(3767621, 1, "4cc4ebe4344936b5afd276844decdf6ea4045a4f5db79e757fd058fb64db3449") -- Depot 3767621
+setManifestid(3767621, "3865972855578857616", 864406771)
