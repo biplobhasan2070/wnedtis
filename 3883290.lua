b8aa9e2f04e03335b5bff557e752acd70215be4e
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-20 17:43:28 +0600

    Add 3883290.lua for AppID 3883290

---
+++ 3883290.lua
@@ -0,0 +1,13 @@
+-- 3883290's Lua and Manifest Created by Hubcap Manifest
+-- LABYRINTHOS: The Depths Want You
+-- Created: August 20, 2026 at 06:17:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3883290) -- LABYRINTHOS: The Depths Want You
+addtoken(3883290, "1412171111747833619")
+-- MAIN APP DEPOTS
+addappid(3883291, 1, "4a6a228ca523c82c868a2c01b95a71214020c31638a9fcf85170ead5d08af4ca") -- Depot 3883291
+setManifestid(3883291, "576579577526937409", 1333063626)
