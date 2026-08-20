6502e2b3fa619325c81eb6e60b4780d8b6c62d07
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-20 17:43:27 +0600

    Add 4970120.lua for AppID 4970120

---
+++ 4970120.lua
@@ -0,0 +1,14 @@
+-- 4970120's Lua and Manifest Created by Hubcap Manifest
+-- Museum of Lost Dinosaurs
+-- Created: August 20, 2026 at 00:01:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4970120) -- Museum of Lost Dinosaurs
+-- MAIN APP DEPOTS
+addappid(4970121, 1, "dac7a58916a5354162b0dca8d5262087f318eb8aafc7ab85db6418e5949ba5df") -- Depot 4970121
+setManifestid(4970121, "6324038564324937373", 121698862)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4970129) -- Depot 4970129 (empty depot)
