ae5e84e3da1c3cf4ad9b807ef988a31b2726ab3e
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-22 12:28:41 +0600

    Add 4001890.lua for AppID 4001890

---
+++ 4001890.lua
@@ -0,0 +1,5 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(4001890)
+addappid(4001891, 1, "b5c8ad37740a8db6d370a6934bf203cfaeb3343003e6277ffc96c63f4df2b7ca")
+setManifestid(4001891, "5639719782218182861", 641712844)
+-- Discord: https://discord.gg/Xx2qdXCvKY
