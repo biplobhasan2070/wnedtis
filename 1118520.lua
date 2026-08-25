c018f2431aa160e8776a36be1c7190483ab69872
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-25 16:04:06 +0600

    Add 1118520.lua for AppID 1118520

---
+++ 1118520.lua
@@ -0,0 +1,7 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(1118520, 1, "e52365d9e88efee525a3978c4991cad62a7f1bf6a53054dc6b38df387bc8dece")
+addappid(1118521, 1, "20543823281f3ef968fcd728fafb6786a33991bb918fbce88cd0a5c7fa49eb5a")
+setManifestid(1118521, "288540036789973605", 8409178518)
+addappid(1118522, 1, "e4e1d8088155be0d69686a0d236bb8693beede9db841019973b75aa8c8677ecd")
+setManifestid(1118522, "8156106585199049665", 8432418807)
+-- Discord: https://discord.gg/Xx2qdXCvKY
