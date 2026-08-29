716f8f837e5220d4a341fdc5e9fea93386da6a74
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-29 15:12:29 +0600

    Add 4187050.lua for AppID 4187050

---
+++ 4187050.lua
@@ -0,0 +1,5 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(4187050)
+addappid(4187051, 1, "67665c5fcee5e171a3d5ab2dd829a6e06638a731acfc26e834aef04897b59d77")
+setManifestid(4187051, "3490354927824032198", 389389715)
+-- Discord: https://discord.gg/Xx2qdXCvKY
