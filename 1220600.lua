96ada95ae0904fe14333a32b0473713db6a60949
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-31 08:11:01 +0600

    Add 1220600.lua for AppID 1220600

---
+++ 1220600.lua
@@ -0,0 +1,5 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(1220600, 1, "cb518734751d9277070b461d66f476a10f739431e283f73e6f701288861a2b80")
+addappid(1220601, 1, "b9d1b893deb43b0fcc6b2399d002965ce4870d9749a8dddcb46b61427bda9a00")
+setManifestid(1220601, "8338737288068248248", 3473062380)
+-- Discord: https://discord.gg/Xx2qdXCvKY
