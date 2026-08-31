394a5b32029c65b0a31daad765669f862a4d5512
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-09-01 05:47:16 +0600

    Add 5060030.lua for AppID 5060030

---
+++ 5060030.lua
@@ -0,0 +1,5 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(5060030)
+addappid(5060031, 1, "14a93980b4cb30452b931a221b73c3f5bd0df9af791c5b9eab617f3a2c6e0f7c")
+setManifestid(5060031, "4633297026822903980", 1573197256)
+-- Discord: https://discord.gg/Xx2qdXCvKY
