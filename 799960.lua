ae53fd269ee3084d03b2dd598a66187880d6b362
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-31 08:11:31 +0600

    Add 799960.lua for AppID 799960

---
+++ 799960.lua
@@ -0,0 +1,8 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(799960, 1, "76a05bd2ea9bb2ec548717fb60dc1df20e95dec44de99dd88dcaab56e46adbe4")
+addappid(799961, 1, "93413f2290547916282f0371457d5d5e7ec49a446bc419efe75b9ec28558756a")
+setManifestid(799961, "8186039443485463111", 20003873885)
+addappid(799963, 1, "f38abce6bcbceee846ca4bc6238a91a104d5f55b8728e91d001f8503b429123f")
+setManifestid(799963, "5190900035384540036", 39429310713)
+addappid(4014370)
+-- Discord: https://discord.gg/Xx2qdXCvKY
