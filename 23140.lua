49aa70235efff0a88f1131cae544b40bb89ab225
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-08-31 08:11:10 +0600

    Add 23140.lua for AppID 23140

---
+++ 23140.lua
@@ -0,0 +1,7 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(23140)
+addappid(23142, 1, "17e56ec8a5502697f25d4c673e45c843af2dc5afef95a785dc28017cea4cd487")
+setManifestid(23142, "8410624782709167701", 82509830)
+addappid(23141, 1, "c4d233d8392754e2d6644efb180ae25247b3acaf30c9ba295edec612993515b2")
+setManifestid(23141, "3628834186816250380", 83658112)
+-- Discord: https://discord.gg/Xx2qdXCvKY
