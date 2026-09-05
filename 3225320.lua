a0c458d132d081af67d2865c008db980871856b3
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-09-05 07:02:46 +0600

    Add 3225320.lua for AppID 3225320

---
+++ 3225320.lua
@@ -0,0 +1,5 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(3225320, 1, "d4d571ac48f425d920d2d9a2f8d842a0375ad7925ad68bb73220ccdbbe024199")
+addappid(3225321, 1, "30af26836caaa93a0debc841de6086ccdef5cebcf05f2ee66ff60fd942bcb78d")
+setManifestid(3225321, "4146787806707842566", 753244515)
+-- Discord: https://discord.gg/Xx2qdXCvKY
