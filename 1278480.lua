6face4917680a90b72888070bbc89b058f6a328a
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-09-12 13:51:17 +0600

    Add 1278480.lua for AppID 1278480

---
+++ 1278480.lua
@@ -0,0 +1,7 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(1289950,0,"51844f98d8980d90abb687d240bb4e58c4a7b5207e74a384cb63c844d1540e7f")
+setManifestid(1289950,"3907069322538973504")
+addappid(1278481,0,"0ebc750e269131fbcd12e8224bb640d250c1b390ccdde156572413379f9fbd94")
+setManifestid(1278481,"7492305393697515508")
+addappid(1278480)
+-- Discord: https://discord.gg/Xx2qdXCvKY
