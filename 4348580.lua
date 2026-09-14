fece13569ac19e4faa5f63aa1f8a207388477d4d
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-09-14 12:20:23 +0600

    Add 4348580.lua for AppID 4348580

---
+++ 4348580.lua
@@ -0,0 +1,5 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(4348580)
+addappid(4348581,0,"57b8dc8d7c7ebdba3317f664c1e6175330c425f57f872206b608dac3c9ee954e")
+setManifestid(4348581,"1254334540840484259")
+-- Discord: https://discord.gg/Xx2qdXCvKY
