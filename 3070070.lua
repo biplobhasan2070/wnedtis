cbc603000e3579669dfd0000badb8d52ca0da99b
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-09-15 09:17:57 +0600

    Add 3070070.lua for AppID 3070070

---
+++ 3070070.lua
@@ -0,0 +1,5 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(3070070, 1, "1d1ae53a09c22e79300f60f3a738e1b3bc390c7d9e6ced65e7f6b09a03bf9124")
+addappid(3070071, 1, "5c49dfd1ba36d4e81f59ad3a2cd714ec14bdb40ac41848727aca2dc9ed29c748")
+setManifestid(3070071, "7079531578204338127", 6001492923)
+-- Discord: https://discord.gg/Xx2qdXCvKY
