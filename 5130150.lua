d19cff2a02ca534afd91cb91cfe98593c465c33b
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-09-16 07:44:06 +0600

    Add 5130150.lua for AppID 5130150

---
+++ 5130150.lua
@@ -0,0 +1,5 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(5130150, 1, "0e0c21579918fd0a6954f429fbf43687c0fe0b2b704558ab21e4cb46d0b0d4aa")
+addappid(5130151, 1, "2044e2444dca8cff5d0d07d9575c6acbe862886fb0e73ebe9bd695758ffc1e2b")
+setManifestid(5130151, "1941515371035993707", 324669962)
+-- Discord: https://discord.gg/Xx2qdXCvKY
