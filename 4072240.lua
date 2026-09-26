3fb5253ab2d12f212acc69652d0f626ca2a11d2f
biplobhasan2070 <45775937+biplobhasan2070@users.noreply.github.com>
2026-09-26 16:53:15 +0600

    Add 4072240.lua for AppID 4072240

---
+++ 4072240.lua
@@ -0,0 +1,9 @@
+-- Website: https://steamtools.manifesthub.uk
+addappid(4072240)
+addappid(4072241, 1, "808a4827c93d4fda8bd91fdb1b7add1bf5bfd19f652e06ae3f112e3dc4b93363")
+setManifestid(4072241, "1724027282259646520", 6546559240)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
+setManifestid(228990, "1829726630299308803", 102931551)
+-- Discord: https://discord.gg/Xx2qdXCvKY
